971761947a072d5aca49bf77d18cdc325b81f809
Rodrigo <rodrigo127001@gmail.com>
2020-08-27 10:41:09 -0500

    Added products query and view

---
+++ src/pages/productos.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Products from '../components/Products';
+import { graphql } from 'gatsby';
+
+export default (props) => {
+    return(
+        <Layout>
+            <Products products={props.data.allStripeProduct.nodes} />
+        </Layout>
+    )
+};
+
+export const pageQuery = graphql`
+        {
+            allStripeProduct {
+                nodes {
+                    id
+                    name
+                    images
+                }
+            }
+        }
+  
+`
