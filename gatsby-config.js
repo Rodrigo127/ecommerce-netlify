9b526f47eb68614c492ea228db8e162b237aa20d
Rodrigo <rodrigo127001@gmail.com>
2020-08-27 10:19:35 -0500

    Added and configured plugin for stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ["Product", "Price"],
+        secretKey: process.env.STRIPE_SECRET_KEY,
+        downloadFiles: true
+      }
+    },
   ],
   siteMetadata: {
     title: "Sillas Kentucky",
