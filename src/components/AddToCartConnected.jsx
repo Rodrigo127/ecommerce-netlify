e522cb30f834d1ea9cce41ebdb720a540209b982
Rodrigo <rodrigo127001@gmail.com>
2020-08-31 02:01:48 -0500

    Removed a broken export

--- src/components/AddToCartConnected.jsx
+++ src/components/AddToCartConnected.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect, useDispatch} from "react-redux";
-import {addToCart, addItem} from "../state/actions/shopping_cart";
+import {addItem} from "../state/actions/shopping_cart";
 import AddToCart from "./AddToCart";
 
 const AddToCartBtn = ({ sku }) => {
