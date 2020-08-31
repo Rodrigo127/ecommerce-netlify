f95029fbd13aab26b70ab8a7a2aeaf610686d43a
Rodrigo <rodrigo127001@gmail.com>
2020-08-31 10:40:17 -0500

    Added switch to reducer instead of if

--- src/state/reducers/shopping_cart.js
+++ src/state/reducers/shopping_cart.js
@@ -12,34 +12,34 @@ import { ADD_ITEM,CLEAR_SHOPPING_CART,DUMMY_ACTION } from "../actionTypes";
  export default function(state = {}, action){
     const {shopping_cart} = state;
 
-    if(action.type === ADD_ITEM){
-        const productIfAny = shopping_cart.find(product => product.id === action.payload.id);
-        if(productIfAny){
-            const new_state = {shopping_cart: shopping_cart.map( product => {
-                if(product.id === productIfAny.id) return { ...product, quantity: product.quantity + 1 }
-                return product;
-            })};
-           return new_state;
-        }
-
-        const new_state = { shopping_cart: shopping_cart.concat([
-            {
-                ...action.payload,
-                quantity: 1
+    switch (action.type){
+        case ADD_ITEM:
+            const productIfAny = shopping_cart.find(product => product.id === action.payload.id);
+            if(productIfAny){
+                const new_state = {shopping_cart: shopping_cart.map( product => {
+                    if(product.id === productIfAny.id) return { ...product, quantity: product.quantity + 1 }
+                    return product;
+                })};
+               return new_state;
             }
-        ])};
-        return new_state;
+    
+            const new_state = { shopping_cart: shopping_cart.concat([
+                {
+                    ...action.payload,
+                    quantity: 1
+                }
+            ])};
+            return new_state;
+
+        case CLEAR_SHOPPING_CART:
+            return { shopping_cart: [] };
+
+        case DUMMY_ACTION:
+            console.log("executing dummy action");
+            console.log(action.payload);
+            return state;
+
+        default:
+            return { shopping_cart: shopping_cart };
     }
-
-    if(action.type === CLEAR_SHOPPING_CART){
-        return { shopping_cart: [] };
-    }
-
-    if(action.type === DUMMY_ACTION){
-        console.log("executing dummy action");
-        console.log(action.payload);
-        return state;
-    }   
-
-    return { shopping_cart: shopping_cart };
 }
